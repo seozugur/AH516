fa1092e11f52f8a2c24626e617e62b64658fc9a9
orthrin <gronzelg@gmail.com>
2019-12-31 11:40:40 +0300

    content Test

--- test/test.js
+++ test/test.js
@@ -1,23 +1,28 @@
-class CadCursor {
+export class RangeSlider {
 
-    constructor () {
-        this.cadCursorEl = document.getElementById('cadCursor');
-        this.topEl = document.querySelectorAll('#cadCursor .top')[0];
-        this.leftEl = document.querySelectorAll('#cadCursor .left')[0];
-        this.pickerEl = document.querySelectorAll('#cadCursor .picker')[0];
+    // ==== Variables
+    // @param id:string => ID of the DOM element where you want to display rangeslider. Required
+    // @param range:object => define max and min range. default {min: 0, max: 100}
+    // @param width:number => specify the width of the rangeslider. Default 150px
+    // @param handles:array => Number of handles you need maximum 10 handles allowed
+    // => Default [0] specify the position of handles in array by comma separated 
+    // @param displayValues => True to make valuse visible over the handle , false to makes values invisible 
+    // => Default false;
+
+    // ==== Constructor
+    constructor(options) {
+        
+        // Get Container
+        this.id = options.id;
+        this.container = document.getElementById(this.id);
+
+        // Set Default values
+        this.range = (options.range) ? options.range : {min:0, max:100};
+        this.width = (options.width) ? options.width : 150;
+        this.handles = (options.handles) ? options.handles : [0];
+        this.displayValue = (options.displayValue) ? options.displayValue : false;
+
+        this.init();
+
     }
-
-    move (positionX, positionY) {
-        this.topEl.style.transform = 'translateY(' + positionY + 'px)';
-        this.leftEl.style.transform = 'translateX(' + positionX + 'px)';
-        this.pickerEl.style.transform = 'translate(' + positionX + 'px, ' + positionY + 'px)';
-    }
-
 }
-
-const cadCursor = new CadCursor();
-
-document.addEventListener('mousemove', (event) => {
-   cadCursor.move(event.clientX, event.clientY);
-});
-
