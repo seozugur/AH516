55eda83576939a0d3e789f54552a33cc7d92c837
orthrin <gronzelg@gmail.com>
2020-01-04 13:46:52 +0300

    slider Tryout 1

---
+++ docs/scripts/jq-slider.js
@@ -0,0 +1,10 @@
+$('.nstSlider').nstSlider({
+    "crossable_handles": false,
+    "left_grip_selector": ".leftGrip",
+    "right_grip_selector": ".rightGrip",
+    "value_bar_selector": ".bar",
+    "value_changed_callback": function(cause, leftValue, rightValue) {
+        $(this).parent().find('.leftLabel').text(leftValue);
+        $(this).parent().find('.rightLabel').text(rightValue);
+    }
+});
